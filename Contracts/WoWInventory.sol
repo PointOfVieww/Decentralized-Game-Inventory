03c9e968a4c56d5278d17be624997f18c03d7b94
PavelDochev <pointofvieww@gmail.com>
2018-03-03 21:52:21 +0200

    added functionality to buy gold silver and copper
    maximum to buy at once is 99.9999 ether and minimum is 0.0001 as 0.0001 is 1 copper after someone get 100+ silver or copper it is being distributed as 1 gold and 1 silver respectively

--- Contracts/WoWInventory.sol
+++ Contracts/WoWInventory.sol
@@ -14,8 +14,9 @@ contract WoWInventory {
     uint private oneGoldCoin = 1 ether;
     uint private oneSilverCoin = oneGoldCoin / 100;
     uint private oneCopperCoin = oneSilverCoin / 100;
+    
 
-    address private owner;
+    address public owner;
     mapping (address=>uint256) gold;
     mapping (address=>uint256) silver;
     mapping (address=>uint256) copper;
@@ -34,22 +35,86 @@ contract WoWInventory {
     function WoWInventory() public {
         owner = msg.sender;
     }
+    function numDigits(uint number) public pure returns(uint numberOfDigits) {
+        while (number > 0) {
+            number /= 10;
+            numberOfDigits++;
+        }
+        return numberOfDigits;
+    }
+    //calculate which coins to get when sent more than 1 eth
+    function calculateGoldToReceive(uint value) private pure returns(uint) {
+        return value / 10000;
+    }
+    function calculateSilverToReceive(uint value) private pure returns(uint) {
+        return (value / 100) % 100;
+    }
+    function calculateCopperToReceive(uint value) private pure returns(uint) {
+        return value % 100;
+    }
 
-    function buyCoins() public payable {
+    function buyCoins() public payable returns(uint[3]) {
         //only more than 0.0001 eth
         require(msg.value > (1 ether / 10000));
-        if (msg.value > 1 ether) {
-            //buy gold
+        //max 99.9999 ether
+        require(msg.value < 100 ether);
+        //if numOfDigits > 6 ether is > 100
+        //if numOfDigits < 1 ethere is < 0.0001
+        uint value = msg.value / 10**14;
+        uint numOfDigits = numDigits(value);
+        assert(numOfDigits <= 6);
+        assert(numOfDigits >= 1);
+        uint[3] memory res;
+        uint goldToReceive = 0;
+        uint silverToReceive = 0;
+        uint copperToReceive = 0;
+
+        //there will be gold
+        if (numOfDigits == 6 || numOfDigits == 5) {
+            goldToReceive = calculateGoldToReceive(value);
+            silverToReceive = calculateSilverToReceive(value);
+            copperToReceive = calculateCopperToReceive(value);
+
+        } else if (numOfDigits == 4 || numOfDigits == 3) {
+            //there may be silver and copper
+            silverToReceive = value / 100;
+            copperToReceive = calculateCopperToReceive(value);
+        } else if (numOfDigits == 2 || numOfDigits == 1) {
+            //Only copper
+            copperToReceive = value;
         }
-        //if > 0.01 ether && < 1 ether
-        if (msg.value > (1 ether / 100) && msg.value < 1 ether) {
-            //buy silver
-        }
-        if (msg.value > (1 ether / 10000) && msg.value < (1 ether / 100)) {
-            //buy copper
-        }
+        checkIfCoinsGoOverLimit(msg.sender,silverToReceive,copperToReceive);
+
+        res[0] = goldToReceive;
+        res[1] = silverToReceive;
+        res[2] = copperToReceive;
+
+        gold[msg.sender] += goldToReceive;
+        
         coinHolders[msg.sender] = true;
-        BoughtCoins(msg.sender,msg.value);    
+        BoughtCoins(msg.sender,msg.value);
+        return res;
+    }
+
+    function checkIfCoinsGoOverLimit(address addr,uint _silver,uint _copper) internal {
+        uint256 currSilver = silver[addr];
+        uint currCopper = copper[addr];
+        uint temp = 0;
+        if (currSilver + _silver > 100) {
+            temp = (currSilver + _silver) - 100;
+            gold[addr] += 1;
+            silver[addr] = temp;
+        } else {
+            silver[addr] += _silver;
+        }
+
+        if (currCopper + _copper > 100) {
+            temp = (currCopper + _copper) - 100;
+            silver[addr] += 1;
+            copper[addr] = temp;
+        } else {
+            copper[addr] += _copper;
+        }
     }
 
     function buyItem(string itemName) public isCoinHolder {
--- test/WoWInventory.test.js
+++ test/WoWInventory.test.js
@@ -1,5 +1,27 @@
 const WoWInventory = artifacts.require("./WoWInventory.sol");
 
-contract('WoWInventory',function(){
-    
+contract('WoWInventory',function(accounts){
+    let inventoryInstance;
+    let owner = accounts[0];
+    describe("creating WoW inventory",() => {
+        beforeEach(async function() {
+            inventoryInstance = await WoWInventory.new({
+                from:owner
+            });
+        });
+
+        it("should be correct owner", async function() {
+            let _owner = await inventoryInstance.owner.call();
+            assert.strictEqual(_owner,owner,"expected owner");
+        });
+
+        it("should return correct gold", async function() {
+            let arrayWithCoins = await inventoryInstance.buyCoins({value:5});
+
+            assert.Equal(arrayWithCoins[0],5,"The expected gold is not set");
+
+            // assert.Equal(arrayWithCoins[1],23,"The expected silver is not set");
+            // assert.Equal(arrayWithCoins[2],30,"The expected copper is not set");
+        });
+    });
 })
